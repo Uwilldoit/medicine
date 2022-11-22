c0bf79f9269778f7159ca2abd5740af415cf03ec
WANG <1243189230@qq.com>
2022-11-22 11:28:58 +0800

    供应商

---
+++ src/main/java/com/wang/pojo/Provider.java
@@ -0,0 +1,12 @@
+package com.wang.pojo;
+
+import lombok.Data;
+
+@Data
+public class Provider {
+    private Integer id;
+    private String name;
+    private String position;
+    private String phone;
+    private String description;
+}
